7e13e22b903a66ccafecf66804653af91a74d774
YIRAN\lyr20 <lyr2011010733@126.com>
2024-12-17 17:49:25 +0800

    前端-登录窗口50%

---
+++ frontend/src/views/Header/HeaderView.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <span class="title">数据可视化</span>
+    <div class="login">
+      <span class="sign-in" @click="signIn">登录</span>
+      <span>注册</span>
+    </div>
+  </header>
+  <SignInView ref="signInView" />
+</template>
+
+<script setup lang="ts">
+import SignInView from "./SignInView.vue";
+import { ref } from "vue";
+const signInView = ref();
+const signIn = function () {
+  signInView.value.dialogVisible = true;
+};
+</script>
+
+<style lang="less" scoped>
+header {
+  width: 100%;
+  min-height: 60px;
+  background: url(@/assets/images/title.png) no-repeat center;
+  background-size: 100% 100%;
+  color: #19ecff;
+  font-size: 40px;
+  text-align: center;
+  position: relative;
+  .login {
+    position: absolute;
+    font-size: 16px;
+    top: 45px;
+    right: 30px;
+    color: #fff;
+    .sign-in {
+      padding-right: 20px;
+      margin-right: 20px;
+      border-right: 1px solid #19ecff;
+    }
+    span {
+      cursor: pointer;
+    }
+    span:hover {
+      color: #00b498;
+    }
+  }
+}
+</style>
---
+++ frontend/src/views/Header/SignInView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <el-dialog
+      v-model="dialogVisible"
+      width="400"
+      top="40vh"
+      :show-close="false"
+    >
+      <div class="sign-in-container">
+        <el-form>
+          <el-form-item>
+            <el-input
+              placeholder="请输入账号"
+              :prefix-icon="UserName"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              placeholder="请输入密码"
+              :prefix-icon="Password"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="sign-btn">
+
+        </div>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineExpose } from "vue";
+import UserName from "@/assets/images/UserName.vue";
+import Password from "@/assets/images/Password.vue";
+const dialogVisible = ref(false);
+defineExpose({ dialogVisible });
+</script>
+
+<style lang="less" scoped>
+:deep(.el-dialog) {
+  background: url(@/assets/images/signIn.webp) no-repeat center;
+  background-size: 100% 100%;
+  .sign-in-container {
+    width: 250px;
+    margin: 0 auto;
+    .el-input__wrapper {
+      background: url(@/assets/images/input.png) no-repeat center;
+      background-size: 100% 100%;
+      box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color))
+        inset;
+    }
+    .sign-btn{
+      width: 200px;
+      height: 30px;
+      margin: 0 auto;
+      background: url(@/assets/images/signbtn.png) no-repeat center;
+      background-size:100% 100%;
+    }
+  }
+}
+</style>
