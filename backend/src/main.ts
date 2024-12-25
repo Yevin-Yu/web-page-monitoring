a4c88f6e45fb4598d862ec803e7604f6a87b7bd2
YIRAN\lyr20 <lyr2011010733@126.com>
2024-12-25 17:20:04 +0800

    [feat][20241225][后端-处理跨域]

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 引入全局异常过滤器
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: '*', // 允许的来源
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 允许的 HTTP 方法
+    credentials: true, // 是否允许携带凭证
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
