7a025f84373bca9612383b46ca31e9f6686a1a7b
YIRAN\lyr20 <lyr2011010733@126.com>
2025-02-11 17:47:49 +0800

    [feat][20250211][前端-新增工作台地图及折柱混合图]

---
+++ frontend/src/views/Workbench/LineHisView.vue
@@ -0,0 +1,121 @@
+<template>
+  <div ref="linHisChart" class="line-his-chart">
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts';
+const linHisChart = ref()
+const init = function () {
+  if (linHisChart.value) {
+    const myChart = echarts.init(linHisChart.value);
+    let option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          crossStyle: {
+            color: '#999'
+          }
+        }
+      },
+      // legend: {
+      //   data: ['Evaporation', 'Precipitation', 'Temperature']
+      // },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisPointer: {
+            type: 'shadow'
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: 'Precipitation',
+          min: 0,
+          max: 250,
+          interval: 50,
+          axisLabel: {
+            formatter: '{value} ml'
+          },
+        },
+        {
+          type: 'value',
+          name: 'Temperature',
+          min: 0,
+          max: 25,
+          interval: 5,
+          axisLabel: {
+            formatter: '{value} °C'
+          }
+        }
+      ],
+      series: [
+        {
+          name: 'Evaporation',
+          type: 'bar',
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + ' ml';
+            }
+          },
+          data: [
+            2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+          ],
+          itemStyle: {
+            borderRadius: [5, 5, 5, 5],
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "#195292" }, { offset: 1, color: "#0197A1" }
+            ])
+          }
+        },
+        {
+          name: 'Precipitation',
+          type: 'bar',
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + ' ml';
+            }
+          },
+          data: [
+            2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+          ],
+          itemStyle: {
+            borderRadius: [5, 5, 5, 5],
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "#0372FB" }, { offset: 1, color: "#92C3FF" }
+            ])
+          }
+        },
+        {
+          name: 'Temperature',
+          type: 'line',
+          yAxisIndex: 1,
+          tooltip: {
+            valueFormatter: function (value) {
+              return value + ' °C';
+            }
+          },
+          data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
+          itemStyle:{
+            color:'#1BD4E7'
+          }
+        }
+      ]
+    };
+    myChart.setOption(option);
+  }
+}
+onMounted(() => {
+  init()
+})
+</script>
+<style lang="less" scoped>
+.line-his-chart {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- frontend/src/views/Workbench/WorkbenchView.vue
+++ frontend/src/views/Workbench/WorkbenchView.vue
@@ -1,3 +1,95 @@
 <template>
-  <h1 style="color: #fff;">工作台</h1>
+  <div class="work-bench">
+    <div id="map-container"></div>
+    <div class="his-line">
+      <LineHisView />
+    </div>
+  </div>
+
 </template>
+<script setup>
+import AMapLoader from '@amap/amap-jsapi-loader';
+import { ref, onMounted } from 'vue'
+import LineHisView from './LineHisView.vue';
+const initMap = () => {
+  AMapLoader.load({
+    key: "a746947369b56c4abbadd27235900e86",
+    version: "2.0",
+    plugins: [],
+    Loca: {
+      "version": '2.0.0'
+    },
+  }).then((AMap) => {
+    var map = new AMap.Map('map-container', {
+      zooms: [4, 8],
+      zoom: 4.5,
+      showLabel: false,
+      viewMode: '3D',
+      center: [105.425968, 35.882505],
+      mapStyle: 'amap://styles/dark',
+    });
+
+    var loca = new Loca.Container({
+      map,
+      opacity: 1,
+    });
+
+    var geo = new Loca.GeoJSONSource({
+      url: 'https://a.amap.com/Loca/static/loca-v2/demos/mock_data/cuisine.json',
+    });
+
+    var pl = window.pl = new Loca.PointLayer({
+      zIndex: 10,
+      blend: 'lighter',
+    });
+
+    var style = {
+      radius: 3.5,
+      unit: 'px',
+      color: '#3C1FA8',
+      borderWidth: 0,
+      blurWidth: 3.5,
+    }
+    pl.setSource(geo);
+    pl.setStyle(style);
+    loca.add(pl);
+
+    pl.addAnimate({
+      key: 'radius',
+      value: [0, 1],
+      duration: 500,
+      easing: 'Linear',
+      transform: 2000,
+      random: true,
+      delay: 8000,
+      yoyo: true,
+      repeat: 100000
+    });
+  }).catch(e => {
+    console.error(e);
+  })
+}
+onMounted(() => {
+  initMap();
+});
+</script>
+<style lang="less" scoped>
+.work-bench {
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  #map-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  .his-line {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 500px;
+    height: 300px;
+  }
+}
+</style>
